88c60755881f092c18f4e7d215bba4d2bf5a35c0
Gantlas <3glifeyy@gmail.com>
2020-09-08 04:07:15 +0300

    v2.0

--- index.js
+++ index.js
@@ -33,7 +33,10 @@ const menu = [
       {
         name: "Пункт 1.3",
         submenu: [
-          { name: "Пункт 1.3.1" },
+          {
+            name: "Пункт 1.3.1",
+            submenu: [{ name: "Пункт 1.3.1.1" }, { name: "Пункт 1.3.1.2" }],
+          },
           {
             name: "Пункт 1.3.2",
             submenu: [{ name: "Пункт 1.3.2.1" }, { name: "Пункт 1.3.2.2" }],
@@ -56,22 +59,22 @@ const menu = [
   },
 ];
 
-let i = 0;
 const displayMenu = (menu) => {
   const menuNode = document.querySelector(".menu");
+
   menu.forEach((el) => {
     const menuItem = document.createElement("li");
     menuItem.classList.add("menu__item");
     menuItem.innerHTML = el.name;
     menuNode.append(menuItem);
+
     if (el.submenu) {
       menuItem.addEventListener("mouseenter", () => {
         displaySubmenu(menuItem, el.submenu);
-        console.log(i++);
       });
+
       menuItem.addEventListener("mouseleave", () => {
         hideSubmenu(menuItem);
-        console.log(i++);
       });
     }
   });
@@ -81,19 +84,25 @@ const displaySubmenu = (itemNode, submenu) => {
   const submenuNode = document.createElement("ul");
   submenuNode.classList.add("submenu");
   itemNode.append(submenuNode);
+
+  if (itemNode.closest(".submenu")) {
+    submenuNode.style.left = "calc(100% + 1px)";
+    submenuNode.style.top = "-4px";
+  }
+
   submenu.forEach((el) => {
     const submenuItem = document.createElement("li");
     submenuItem.classList.add("submenu__item");
     submenuItem.innerHTML = el.name;
     submenuNode.append(submenuItem);
+
     if (el.submenu) {
       submenuItem.addEventListener("mouseenter", () => {
         displaySubmenu(submenuItem, el.submenu);
-        console.log(i++);
       });
+
       submenuItem.addEventListener("mouseleave", () => {
         hideSubmenu(submenuItem);
-        console.log(i++);
       });
     }
   });
